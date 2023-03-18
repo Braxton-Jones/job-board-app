dd11c4d5d08e7e1569830982f5d770ad8b0b81e1
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-03-17 21:14:50 -0400

    "update"

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "job-board-app"
+  base: "/job-board-app/"
   
 })
